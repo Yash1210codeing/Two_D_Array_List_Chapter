1b56cb5d999d0f915882cb26970caa9917aa8458
Yash Raj Sharma <154724719+Yash1210codeing@users.noreply.github.com>
2024-07-09 22:11:36 +0530

    Add files via upload

---
+++ Two_D_Array_List_Chapter/Two_D_Array_List_Demo.java
@@ -0,0 +1,11 @@
+package Two_D_Array_List_Chapter;
+import java.util.*;
+public class Two_D_Array_List_Demo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        List<List<Integer>>ans=new ArrayList<List<Integer>>();
+        List<Integer>ans1=new ArrayList<Integer>();
+        System.out.println(ans);
+        System.out.println(ans1);
+	}
+}
